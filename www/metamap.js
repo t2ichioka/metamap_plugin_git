ba316c8512f265712733636dc6b64990a2bdb953
t2ichioka <t2ichioka@jst.go.jp>
2026-09-10 10:14:07 +0900

    0.0.44

--- www/metamap.js
+++ www/metamap.js
@@ -1,7 +1,9 @@
 // This is a JavaScript file
+console.log("aaaaaaaaaaaa:MetaMap Start");
 var MetaMap = function(){};
-
+console.log("aaaaaaaaaaaa:MetaMap Init");
 MetaMap.prototype.callMetaMap = function(additionalQuery, language) {
+    console.log("aaaaaaaaaaaa:cordova.exec");
     cordova.exec(
         function(result){console.log("success MetaMap Call result = " + result);},
         function(error){console.log("failure MetaMap Call error = " + error);},
@@ -10,5 +12,8 @@ MetaMap.prototype.callMetaMap = function(additionalQuery, language) {
         [additionalQuery, language]
     );
 };
+console.log("aaaaaaaaaaaa:MetaMap.prototype.callMetaMap");
 var metamap = new MetaMap();
+console.log("aaaaaaaaaaaa:new MetaMap()");
 module.exports = metamap;
+console.log("aaaaaaaaaaaa:module.exports");
