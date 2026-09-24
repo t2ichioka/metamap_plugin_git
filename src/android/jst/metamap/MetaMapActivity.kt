5b6a90112816fc6942a9f9ef1b4025f369aa8329
t2ichioka <t2ichioka@jst.go.jp>
2026-09-24 13:52:24 +0900

    0.0.70

--- src/android/jst/metamap/MetaMapActivity.kt
+++ src/android/jst/metamap/MetaMapActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.MenuItem
+import jst.metamap.R
 import jp.metamaps.mapview.MetamapMapView
 import jp.metamaps.mapview.MetamapMapViewConfiguration
 import jp.metamaps.mapview.MetamapMapViewEvent
